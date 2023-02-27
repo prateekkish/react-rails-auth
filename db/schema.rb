ae3848c7245ca2e518f4fd9a52d63c87b3dd8d9f
Prateek Choudhary <prateekkish@gmail.com>
2023-02-27 17:40:26 +0530

    Switch to MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2023_02_27_053814) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "referrals", force: :cascade do |t|
+  create_table "referrals", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -23,8 +20,8 @@ ActiveRecord::Schema.define(version: 2023_02_27_053814) do
     t.index ["user_id"], name: "index_referrals_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", limit: 191, default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
@@ -33,7 +30,6 @@ ActiveRecord::Schema.define(version: 2023_02_27_053814) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "referrals", "users"
